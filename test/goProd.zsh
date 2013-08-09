49d6f43e14794f7c13c88de012a4009b68699e14
Halil <halil.gamsizkan@cern.ch>
2013-08-09 11:59:50 +0200

    file path fix for producer tester script

--- test/goProd.zsh
+++ test/goProd.zsh
@@ -11,6 +11,9 @@ OUTPUT=Products.${DSET}.root.txt
 for q in `cat root_file_uris/root_files_${DSET}.txt`;do SOURCE="$q"; done #!! only the last file
 rm -f $OUTPUT
 echo $HOST > $OUTPUT
-CMD="./openHLT.py -p -v -t hlt_files/hlt_${DSET}.py -i $SOURCE -o /tmp/halil/Products.${DSET}.root --go 2>&1 | tee $OUTPUT"
+CMD="./openHLT.py -p -v -t hlt_${DSET}.py -i $SOURCE -o /tmp/halil/Products.${DSET}.root --go 2>&1 | tee $OUTPUT"
+rm -f hlt_${DSET}.py
+ln -s hlt_files/hlt_${DSET}.py
 echo $CMD
 eval ${CMD}
+rm -f hlt_${DSET}.py
